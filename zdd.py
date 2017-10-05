be0e597ab956ba37aaeb3430cd2260618940252e
Logan Rakai <lmrakai@gmail.com>
2017-10-05 00:19:27 -0600

    add marathon-lb

--- zdd.py
+++ zdd.py
@@ -32,10 +32,12 @@ def query_yes_no(question, default="yes"):
     # Thanks stackoverflow:
     # https://stackoverflow.com/questions/3041986/python-command-line-yes-no-input
     """Ask a yes/no question via input() and return their answer.
+
     "question" is a string that is presented to the user.
     "default" is the presumed answer if the user just hits <Enter>.
         It must be "yes" (the default), "no" or None (meaning
         an answer is required of the user).
+
     The "answer" return value is True for "yes" or False for "no".
     """
     valid = {"yes": True, "y": True, "ye": True,
